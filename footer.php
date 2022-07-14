992da3685c328e461b7055cb84e9401fe148d1ce
shailyshah99 <72338169+shailyshah99@users.noreply.github.com>
2022-07-14 10:25:25 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="index.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +911111111111 </p>
+            <p> <i class="fas fa-phone"></i> +912222222222 </p>
+            <p> <i class="fas fa-envelope"></i> sh7@gmail.come </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Gujarat, India </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">
+        Self made mini project by: Hetansi Patel & Shaily Shah
+    </span> </div>
+
+</section>
